c3854cff354e2b9d5688b02d19688ed61fac1a81
Vaalarivan2002 <pvaalarivan@gmail.com>
2023-01-19 21:57:24 +0530

    added gitignore

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&h4fczqctdzh4bkuvexb1k!1(#n_s88vzl1=^lutcex+s7-hlv'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
